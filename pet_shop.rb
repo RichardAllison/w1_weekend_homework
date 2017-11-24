533bd70fcd7042d77dc7c6b1606ff32b70852b94
Richard Allison <richard.allison.18@outlook.com>
2017-11-24 14:45:23 +0000

    stock count and pets by breed functions passed tests

--- pet_shop.rb
+++ pet_shop.rb
@@ -17,3 +17,21 @@ end
 def increase_pets_sold(pet_shop, number)
   pet_shop[:admin][:pets_sold] += number
 end
+
+def stock_count(pet_shop)
+  stock = 0
+  for pets in pet_shop[:pets]
+    stock += 1
+  end
+  return stock
+end
+
+def pets_by_breed(pet_shop, breed)
+    pets_by_breed_array = []
+    for pet in pet_shop[:pets]
+      if pet[:breed] == breed
+        pets_by_breed_array.push(pet)
+      end
+    end
+    return pets_by_breed_array
+end
